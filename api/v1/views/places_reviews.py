d8af4855b62e42c3604fb42e0e43767adc0fcf9b
sarahmarkland <sarah.markland@icloud.com>
2023-07-25 15:25:27 -0700

    Revert "gartic phone"
    This reverts commit 18a4e282a432984cfbd529dce336f1a2cc801c13.

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -11,7 +11,16 @@ from flask import jsonify, abort, request
 
 @app_views.route("/places/<place_id>/reviews",
                  methods=["GET"], strict_slashes=False)
-def 
+def retrieves_all_reviews(place_id):
+    """Returns the list of all Reviews objects"""
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+    reviews = place.reviews
+    reviews_list = []
+    for review in reviews:
+        reviews_list.append(review.to_dict())
+    return jsonify(reviews_list)
 
 
 @app_views.route("/reviews/<review_id>", methods=["GET"], strict_slashes=False)
