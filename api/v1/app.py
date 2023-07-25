7378833dd302ca2c275b9a8ae50dda2f18eef6bd
casonbobo <casonsbobo@gmail.com>
2023-07-25 13:57:00 -0700

    changed app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,35 +1,28 @@
 #!/usr/bin/python3
-"""
-Starts up a copy of a flask-app
-"""
-from flask import Flask
+""" Status of your API """
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS
+import os
+from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
-
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
-
 @app.teardown_appcontext
-def teardown_db(self):
-    """Closes the storage on teardown"""
-    from models import storage
+def tear_it_down(stuff):
+    """ clost storage """
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(e):
-    """404 error handler"""
-    return {"error": "Not found"}, 404
+def handle_404(error):
+    """ displays 404 error """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
-def start_flask():
-    """ start flask """
-    from os import getenv as env
-    app.run(host=env('HBNB_API_HOST', default='localhost'),
-            port=env('HBNB_API_PORT'),
-            threaded=True)
-
-
-if __name__ == "__main__":
-    start_flask()
+if __name__ == '__main__':
+    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.environ.get("HBNB_API_PORT", 5000))
+    app.run(host=host, port=port, threaded=True)
