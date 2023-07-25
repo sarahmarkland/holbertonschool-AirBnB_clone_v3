c48680ed07db51cbd8cc5ce09a440be6802dbb86
casonbobo <casonsbobo@gmail.com>
2023-07-25 14:00:06 -0700

    what the fudge

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,7 +10,6 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
-
 @app.teardown_appcontext
 def tear_it_down(stuff):
     """ clost storage """
