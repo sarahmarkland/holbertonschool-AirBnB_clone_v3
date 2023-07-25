5f87fa34de97d65e4c30b745d534152e7bc6df9e
casonbobo <casonsbobo@gmail.com>
2023-07-25 13:32:38 -0700

    argh

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -5,6 +5,8 @@ from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
 from models.place import Place
+from models.city import City
+from models.user import User
 
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'],
